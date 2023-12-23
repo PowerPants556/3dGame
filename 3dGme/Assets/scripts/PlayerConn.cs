9bd2e586ed2c6fb8f21340453bf6a885946fde1b
PowerPants556 <146521497+PowerPants556@users.noreply.github.com>
2023-12-23 12:41:57 +0200

    lessonMovement

--- 3dGme/Assets/scripts/PlayerConn.cs
+++ 3dGme/Assets/scripts/PlayerConn.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 public class PlayerConn : MonoBehaviour
 {
     [SerializeField] private Rigidbody rb;
-    [SerializeField] private animator animator;
+    [SerializeField] private Animator animator;
 
-    private float angle, dirZ, jumpforce = 6f, turnSpeed = 80f;
+    private float angleY, dirZ, jumpforce = 6f, turnSpeed = 80f;
     private bool isGrounded;
     private Vector3 jumpDir;
-    
-   private void FixedUpdate()
+
+    private void FixedUpdate()
     {
-        angleY = Input.GetAxis(*Mouse X*) * Time.fixedDeltaTime * turnSpeed;
-        sirZ = Input.GetAxis(*Vertical*);
+        angleY = Input.GetAxis("MouseX") * Time.fixedDeltaTime * turnSpeed;
+        dirZ = Input.GetAxis("Vertical");
         transform.Rotate(new Vector3(0f, angleY, 0f));
     }
     private void Update()
@@ -23,13 +23,13 @@ public class PlayerConn : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
-               Jump();
+                Jump();
             }
             else
             {
-                animator.SetTrigger(*isLanded *);
+                animator.SetTrigger("isLanded");
             }
-            Move();
+            Move(dirZ, "isWalkForward", "isWalkBack");
             Run();
             Dodge();
         }
@@ -40,23 +40,56 @@ public class PlayerConn : MonoBehaviour
     }
     private void Jump()
     {
-
+        animator.Play("Sword_Jump_Platformer_Start");
+        animator.applyRootMotion = false;
+        jumpDir = new Vector3(0f, jumpforce, dirZ * jumpforce / 2f);
+        jumpDir = transform.TransformDirection(jumpDir);
+        rb.AddForce(jumpDir, ForceMode.Impulse);
+        isGrounded = false;
     }
-    private void Move()
+    private void Move(float dir, string parametrName, string altParamName)
     {
-
+        if (dir > 0)
+        {
+            animator.SetBool(parametrName, true);
+        }
+        else if (dir < 0)
+        {
+            animator.SetBool(altParamName, true);
+        }
+        else
+        {
+            animator.SetBool(parametrName, false);
+            animator.SetBool(altParamName, false);
+        }
     }
     private void Run()
     {
-
+        animator.SetBool("isRun", Input.GetKey(KeyCode.LeftShift));
     }
     private void Dodge()
     {
-
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            animator.Play("Sword_Dodgle_Left");
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            animator.Play("Sword_Dodge_Right");
+        }
     }
     private void MoveInAir()
     {
-
+        if (new Vector2(rb.velocity.x, rb.velocity.z).magnitude < 1.1f)
+        {
+            jumpDir = new Vector3(0f, rb.velocity.y, dirZ);
+            jumpDir = transform.TransformDirection(jumpDir);
+            rb.velocity = jumpDir;
+        }
     }
-    
+    private void OnCollisionEnter(Collision col)
+    {
+        isGrounded = true;
+        animator.applyRootMotion = true;
+    }
 }
