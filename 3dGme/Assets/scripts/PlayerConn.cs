17699d9f22ebd817dc968dfc667a04f5910127d5
PowerPants556 <146521497+PowerPants556@users.noreply.github.com>
2024-02-03 12:33:06 +0200

    PhoneControls

--- 3dGme/Assets/scripts/PlayerConn.cs
+++ 3dGme/Assets/scripts/PlayerConn.cs
@@ -25,6 +25,8 @@ public class PlayerConn : MonoBehaviour
     [HideInInspector]
     public bool isAttacking = false;
 
+    [SerializeField] private Joystick joystickL, joystickR;
+
     private void Start()
     {
         instance = this;
@@ -34,28 +36,24 @@ public class PlayerConn : MonoBehaviour
 
     private void FixedUpdate()
     {
-        angleY = Input.GetAxis("Mouse X") * Time.fixedDeltaTime * turnSpeed;
-        dirZ = Input.GetAxis("Vertical");
+        //angleY = Input.GetAxis("Mouse X") * Time.fixedDeltaTime * turnSpeed;
+        angleY = joystickR.Horizontal * Time.fixedDeltaTime * turnSpeed;
+        //dirZ = Input.GetAxis("Vertical");
+        dirZ = joystickL.Vertical;
         transform.Rotate(new Vector3(0f, angleY, 0f));
     }
     private void Update()
     {
         isAttacking = animator.GetCurrentAnimatorStateInfo(0).IsName("Sword_Attack_R");
-        SwordActivate();
+        
         if (isGrounded)
         {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                Jump();
-            }
-            else
-            {
-                animator.SetTrigger("isLanded");
-            }
+            animator.SetTrigger("isLanded");
             Move(dirZ, "isWalkForward", "isWalkBack");
             Run();
             Dodge();
-            UnequipSword();
+            //UnequipSword();
+            //SwordActivate();
         }
         else
         {
@@ -63,22 +61,25 @@ public class PlayerConn : MonoBehaviour
         }
         //UnequipSword();
     }
-    private void Jump()
+    public void Jump()
     {
-        animator.Play("Sword_Jump_Platformer_Start");
-        animator.applyRootMotion = false;
-        jumpDir = new Vector3(0f, jumpforce, dirZ * jumpforce / 2f);
-        jumpDir = transform.TransformDirection(jumpDir);
-        rb.AddForce(jumpDir * 50, ForceMode.Impulse);
-        isGrounded = false;
+        if (isGrounded)
+        {
+            animator.Play("Sword_Jump_Platformer_Start");
+            animator.applyRootMotion = false;
+            jumpDir = new Vector3(0f, jumpforce, dirZ * jumpforce / 2f);
+            jumpDir = transform.TransformDirection(jumpDir);
+            rb.AddForce(jumpDir * 50, ForceMode.Impulse);
+            isGrounded = false;
+        }
     }
     private void Move(float dir, string parametrName, string altParamName)
     {
-        if (dir > 0)
+        if (dir > 0.3f)
         {
             animator.SetBool(parametrName, true);
         }
-        else if (dir < 0)
+        else if (dir < -0.3f)
         {
             animator.SetBool(altParamName, true);
         }
@@ -90,15 +91,22 @@ public class PlayerConn : MonoBehaviour
     }
     private void Run()
     {
-        animator.SetBool("isRun", Input.GetKey(KeyCode.LeftShift));
+        if (joystickL.Vertical > 0.9f || joystickL.Vertical < -0.9f || Input.GetKey(KeyCode.LeftShift))
+        {
+            animator.SetBool("isRun", true);
+        }
+        else
+        {
+            animator.SetBool("isRun", false);
+        }
     }
     private void Dodge()
     {
-        if (Input.GetKeyDown(KeyCode.A))
+        if (joystickL.Horizontal < -0.8f || Input.GetKeyDown(KeyCode.A))
         {
             animator.Play("Sword_Dodgle_Left");
         }
-        else if (Input.GetKeyDown(KeyCode.D))
+        else if (joystickL.Horizontal > 0.8f || Input.GetKeyDown(KeyCode.D))
         {
             animator.Play("Sword_Dodge_Right");
         }
@@ -161,6 +169,24 @@ public class PlayerConn : MonoBehaviour
         if(col.gameObject.tag == "Weapon" && enemy.isAttacking)
         {
             animator.Play("Sword_Shield_Hit_L_2");
+        }
+    }
+
+    public void OnButtonAttackCicked()
+    {
+        if (isGrounded)
+        {
+            if (!isSwordEquipd)
+            {
+                animator.Play("Equip");
+                isSwordEquipd = true;
+                LastAttackTime = Time.time;
+            }
+            else
+            {
+                animator.Play("Sword_Attack_R");
+                LastAttackTime = Time.time;
+            }
         }
     }
 }
